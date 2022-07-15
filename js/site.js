ce9482d39443d938ce8d8d44e2b2a25613a2c2a4
CGovers <54491781+cg1image@users.noreply.github.com>
2022-07-14 21:01:35 -0700

    Update to Rewind
    created javascript to create a reverse string from input value

--- js/site.js
+++ js/site.js
@@ -1,26 +1,41 @@
+// get the string from the page
+//controller function
+function getvalue(){
 
+    getElementById("alert").classList.add("invisable");
+    
+    let userString=document.getElementById("userString").Value;
 
+    let revString = reverseString(userString);
 
-//get the values from the Page
-//starts or controller function
-function getValues(){
-    //get values from the page
-    let startValue = document.getElementById("startValue")
-
-
-    //we need to call generateNumbers
-
-    //we call displayNumbers
+    displayString(revString);
 }
 
-//generate numbers from startValue to the endValue
-//logic function(s)
-function gnerateNumbers(){
+//Reverse the string
+//logic function
+function reverseString(userString){
 
+    let revString = [];
+
+    //let name = Cliffy
+    //name[0] = "C";
+    //name[5] = "y"
+    //last position in an array name.length - 1
+
+    //reverse a string using a for loop
+    for (let index = userString.length -1; index >= 0; index--) {
+        revString += userString[index];     
+    }
+
+    return revString;
 }
 
-//dispay the numbers and mark even numbers bold
-//display or view functions
-function displayNumbers(){
+//Display the reversed string to the user
+//View Function
+function displayString(revString){
 
+    //write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
